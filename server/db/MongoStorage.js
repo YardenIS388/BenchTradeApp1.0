aa9be40147d88e5c3bedaf033cd36da469ec1e3a
Adva <adva.apelboim@gmail.com>
2022-12-25 20:40:18 +0200

    minor-fix

--- server/db/MongoStorage.js
+++ server/db/MongoStorage.js
@@ -13,7 +13,6 @@ module.exports = class MongoStorage {
   }
 
   connect() {
-    //
     const connectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kts52kd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
     mongoose
       .connect(connectionUrl)
